58dbfed3ea3d24beac98fa47306adf3830ffca62
RupeshMax <rupesh.maxpain@gmail.com>
2020-06-26 22:24:33 +0530

    added

---
+++ 02Basic/marvel.js
@@ -0,0 +1,8 @@
+const superHeroes = ['Iron Man','Spider Man','Captian America','Thor']
+
+console.log(superHeroes)
+console.log(superHeroes[0])
+console.log(superHeroes[2])
+console.log(superHeroes[superHeroes.length - 1]);
+
+console.log(`we have ${superHeroes.length} super heroes`)
