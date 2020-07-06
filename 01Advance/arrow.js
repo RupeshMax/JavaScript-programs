79ad8b3f8da65ffb05f7e9342310c50acad6dc20
RupeshMax <rupesh.maxpain@gmail.com>
2020-07-06 22:09:53 +0530

    advance level 01

--- 01Advance/arrow.js
+++ 01Advance/arrow.js
@@ -1,1 +1,15 @@
- 
+ const sayHello = (name) => `hey there ${name} !`
+
+ console.log(sayHello('ram'))
+
+ const todos = [{
+     title: 'Buy bread',
+     isDone: true,
+ },{
+     title: 'go to gym',
+     isDone: false
+ }]
+
+ const thingsDone = todos.filter((todo) => todo.isDone === true)
+
+ console.log(thingsDone)
